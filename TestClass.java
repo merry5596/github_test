77177f44c860d799b5e20a2073b19cf133867cf3
merry5596 <merry5596@naver.com>
2020-01-14 15:03:34 +0900

    test commit

--- TestClass.java
+++ TestClass.java
@@ -1,5 +1,5 @@
 public class TestClass {
-    public void static main (String[] args) {
-        System.out.println("Hello!");
+    public static void main (String[] args) {
+        System.out.println("Hello!!");
     }
 }
