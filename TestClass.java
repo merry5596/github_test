1b37d894872d1022091085dba75799214b2e16fd
merry5596 <merry5596@naver.com>
2020-01-14 15:31:01 +0900

    branch commit

--- TestClass.java
+++ TestClass.java
@@ -1,7 +1,5 @@
 public class TestClass {
     public static void main (String[] args) {
-        System.out.println("Hello!");
-        System.out.println("World!");
         System.out.println("Hi!");
         System.out.println("Mayeon!");
     }
